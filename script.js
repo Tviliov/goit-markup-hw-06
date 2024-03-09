b4478068417aebf9b327d688b43ef1a20ae2388f
Tsviliov <tsviliov@gmail.com>
2024-03-09 23:43:58 +0200

    added mobile menu

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function openModal() {
     let modal = document.querySelector(".backdrop");
     modal.classList.add("is-open")
 }
+function closeModal() {
+    let modal = document.querySelector(".mobile-menu");
+    modal.classList.remove("is-open")
+}
+function openModal() {
+    let modal = document.querySelector(".mobile-menu");
+    modal.classList.add("is-open")
+}
