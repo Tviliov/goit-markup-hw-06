25b8fe3f253386071888002cda9868e80b2609f8
Tviliov <157270242+Tviliov@users.noreply.github.com>
2024-03-09 19:33:09 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function closeModal() {
+    let modal = document.querySelector(".backdrop");
+    modal.classList.remove("is-open")
+}
+function openModal() {
+    let modal = document.querySelector(".backdrop");
+    modal.classList.add("is-open")
+}
